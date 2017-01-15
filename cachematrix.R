e7095febe20569279089f04fd303fa827dbc37cb
rcarvalheira <rcarvalheira@gmail.com>
2017-01-15 14:59:15 -0200

    Assigment done

--- cachematrix.R
+++ cachematrix.R
@@ -4,14 +4,26 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+    
+    #creatings variable for this environment
     i <- NULL
+    
+    #creating function if change de original variable value is needed
     set <- function(y) {
         x <<- y
         i <<- NULL
     }
+    
+    #retrieve original matrix value
     get <- function() x
+    
+    #save the inverted matrix value
     setInvertMatrix <- function(invMatrix) i <<- invMatrix
+    
+    #Retrieve the inverted matrix value
     getInvertMatrix <- function() i
+    
+    #return a list so that all environment can be accessed outside this function and after it has been used
     list(set = set, get = get,
          setInvertMatrix = setInvertMatrix,
          getInvertMatrix = getInvertMatrix)
@@ -23,10 +35,16 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
     i <- x$getInvertMatrix()
+    
+    # Checks if already has an inverted matrix set
     if(!is.null(i)) {
         message("getting cached data")
+        
+        #Since it is set. Function returns this value and ends
         return(i)
     }
+    
+    #If no inverse set, then get the value, invert the matrix and set to the cache function
     data <- x$get()
     i <- solve(data)
     x$setInvertMatrix(i)
